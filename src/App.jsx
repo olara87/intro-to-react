0faed78ecf2d12f68eb07fd33fe54c13a26a5ae8
omar Lara <omarlara1987@gmail.com>
2023-08-19 05:28:38 -0400

    homepage added to json file

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <p>Hello, World!</p>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
